5139c0617df9581da1b1b36e24c0024a2feb097d
Gabriel <gabrielsantostca@gmail.com>
2025-04-23 22:45:47 -0300

    feat: added output message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,32 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+        final String httpVersion = "HTTP/1.1";
 
-    // Uncomment this block to pass the first stage
+        // Uncomment this block to pass the first stage
 
-    try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            final int statusCode = 200;
+            final String statusPhrase = "OK";
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            Socket clientSocket = serverSocket.accept(); // Wait for connection from client
+            PrintWriter outMessage = new PrintWriter(clientSocket.getOutputStream(), true);
+            String statusLine = httpVersion + " " + statusCode + " " + statusPhrase;
+            outMessage.println(statusLine.concat("\r\n").concat("\r\n"));
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
