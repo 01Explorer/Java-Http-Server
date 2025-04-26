da915cb60dff1a0620fb4e9576150b725876de31
Gabriel <gabrielsantostca@gmail.com>
2025-04-26 16:12:15 -0300

    feat: added structure to handle multiple requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,8 @@
-import enums.ContentType;
-import enums.ResponseStatus;
-import models.Request;
+import models.SocketThread;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,89 +17,13 @@ public class Main {
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
 
-            Socket clientSocket = serverSocket.accept(); // Wait for connection from client
-            PrintWriter outMessage = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            Request request = handleRequestInput(clientSocket.getInputStream());
-            String target = getRequestTarget(request.getRequestLine());
-            String message = switch (target) {
-                case "", "echo", "user-agent" -> buildStatusLine(ResponseStatus.OK);
-                default -> buildStatusLine(ResponseStatus.NOT_FOUND);
-            };
-
-            String echo = "";
-            if (target.equals("echo")) {
-                echo = getEcho(request.getRequestLine());
-            }
-            if (target.equals("user-agent")) {
-                echo = request.getHeaders().get("User-Agent");
+            while (true) {
+               Socket socket = serverSocket.accept();
+               new SocketThread(socket).start();
             }
 
-
-            Map<String, Object> headers = switch (target) {
-                case "echo", "user-agent" -> buildHeader(echo);
-                default -> new HashMap<>();
-            };
-
-            outMessage.println(buildResponse(message, headers, echo));
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
-    }
-
-    private static String getEcho(String request) {
-        request = request.replaceAll("\r\n", "");
-        return request.split(" ")[1].split("/")[2];
-    }
-
-    private static Map<String, Object> buildHeader(String echo) {
-        Map<String, Object> headers = new HashMap<>();
-        headers.put(ContentType.KEY, ContentType.TEXT.getName());
-        headers.put("Content-Length", String.valueOf(echo.length()));
-
-        return headers;
-    }
-
-    private static String buildStatusLine(ResponseStatus responseStatus) {
-        final String httpVersion = "HTTP/1.1";
-       return httpVersion + " " + responseStatus.code + " " + responseStatus.message;
-    }
-
-    private static String buildResponse(String statusLine, Map<String, Object> headers, String body){
-        String headersAsString = headers.entrySet().stream().map(entry -> {
-            return entry.getKey() + ": " + entry.getValue().toString();
-        }).collect(Collectors.joining("\r\n"));
-        return statusLine.concat("\r\n").concat(headersAsString).concat("\r\n\r\n").concat(body);
-    }
-
-    private static String getRequestTarget(String request) {
-        request = request.replaceAll("\r\n", "");
-        String target = request.split(" ")[1].replaceFirst("/", "");
-        if (target.contains("/")) {
-            return request.split(" ")[1].split("/")[1];
-        }
-        return target;
-    }
-
-    private static Request handleRequestInput(InputStream inputStream) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String inputLine;
-        Request.Builder builder = new Request.Builder();
-        List<String> headers = new ArrayList<>();
-        while (reader.ready()) {
-            inputLine = reader.readLine();
-           if(inputLine.contains(": ")){
-               headers.add(inputLine);
-               continue;
-           }
-           if (inputLine.contains("HTTP")){
-               builder.setRequestLine(inputLine);
-               continue;
-           }
-
-           builder.setRequestBody(inputLine);
-        }
-        builder.setHeaders(String.join("\r\n", headers));
-        return builder.build();
     }
 }
