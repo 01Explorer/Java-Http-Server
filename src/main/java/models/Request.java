c1eda68a8fedbc3a741f21de600ca21a1d843bf7
Gabriel <gabrielsantostca@gmail.com>
2025-04-26 14:03:11 -0300

    feat: created Request model for a better approach

---
+++ src/main/java/models/Request.java
@@ -0,0 +1,71 @@
+package models;
+
+import utils.Utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request {
+    private final String requestLine;
+    private final Map<String, String> headers;
+    private final String requestBody;
+
+    private Request(Builder builder) {
+        this.requestLine = builder.requestLine;
+        this.headers = processStringHeaders(builder.headers);
+        this.requestBody = builder.requestBody;
+    }
+
+    private Map<String, String> processStringHeaders(String headersString) {
+        Map<String, String> headers = new HashMap<>();
+        Arrays.stream(headersString.split(Utils.CRLF)).forEach(s -> {
+            String[] keyValuePair = s.trim().split(": ");
+            headers.put(keyValuePair[0], keyValuePair[1]);
+        });
+
+        return headers;
+    }
+
+    public String getRequestLine() {
+        return requestLine;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getRequestBody() {
+        return requestBody;
+    }
+
+    public static Builder getBuilder(){
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String requestLine;
+        private String headers;
+        private String requestBody;
+
+        public Builder setRequestLine(String requestLine) {
+            this.requestLine = requestLine;
+            return this;
+        }
+
+        public Builder setHeaders(String headers) {
+            this.headers = headers;
+            return this;
+        }
+
+        public Builder setRequestBody(String requestBody) {
+            this.requestBody = requestBody;
+            return this;
+        }
+
+        public Request build() {
+            return new Request(this);
+        }
+
+    }
+}
