f17c698b6b54f3fdb54d0bbaa4b6668320c6a8e7
Gabriel <gabrielsantostca@gmail.com>
2025-04-27 16:34:12 -0300

    feat: added create 201 response status

--- src/main/java/enums/ResponseStatus.java
+++ src/main/java/enums/ResponseStatus.java
@@ -3,7 +3,8 @@ package enums;
 public enum ResponseStatus {
 
     OK(200, "OK"),
-    NOT_FOUND(404, "Not Found");
+    NOT_FOUND(404, "Not Found"),
+    CREATED(201, "Created");
 
     public final int code;
     public final String message;
